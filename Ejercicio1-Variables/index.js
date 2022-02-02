5b82254754b760710703839320f760208fc06042
linaespi <linaespi28@hotmail.com>
2022-02-02 00:37:43 -0500

    Ejercicio1

--- Ejercicio1-Variables/index.js
+++ Ejercicio1-Variables/index.js
@@ -11,4 +11,13 @@ const areaBase=16*23;
 const areaTotal= areaLateral+2*areaBase;
 console.log('Area Total', areaTotal + ' cm2');
 
+//AreaTotal para cambiar solo los valores de los lados
+const base=23;
+const altura=13;
+const ancho=16;
 
+const volumen2=base*altura*ancho;
+console.log('Volumen2', volumen2 +' cm3');
+
+const area2=base *altura + 2 * base *ancho;
+console.log('Area2', area2 + 'cm3');
