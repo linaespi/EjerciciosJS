740ac506608dffd72e6f451590b80c0152757b91
linaespi <linaespi28@hotmail.com>
2022-02-01 21:03:59 -0500

    Ejercicio1

---
+++ Ejercicio1-Variables/index.js
@@ -0,0 +1,14 @@
+console.log('hola');
+
+//base 16 *23 cm y altura de 13 cm
+
+const volumen=13*23*16;
+console.log('volumen',volumen + ' cm3');
+
+//Area total= area lateral + 2*area base
+const areaLateral=23*13;
+const areaBase=16*23;
+const areaTotal= areaLateral+2*areaBase;
+console.log('Area Total', areaTotal + ' cm2');
+
+
