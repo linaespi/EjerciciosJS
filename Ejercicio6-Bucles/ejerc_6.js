79b49e232ae2b729642a9043871fe68599b03d71
linaespi <linaespi28@hotmail.com>
2022-02-08 03:56:35 -0500

    Ejercicio_6y_7

---
+++ Ejercicio6-Bucles/ejerc_6.js
@@ -0,0 +1,12 @@
+const notas = [6, 7, 2, 9, 3, 4, 5, 8, 2];
+
+
+let sumaT = 0;
+for (const suma in notas) {
+
+    sumaT += suma;
+
+    console.log('La suma es ' + sumaT)
+}
+
+
---
+++ Ejercicio7-Pares_impares/ejerc_7.js
@@ -0,0 +1,5 @@
+       var numero = prompt("Introduce un número entero");
+       var resultado = parImpar(numero);
+       
+       alert("El numero " + numero + ' es ' + resultado);
+       console.log("El numero " + numero + ' es ' + resultado);
